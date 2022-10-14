9f094aee0e84eaa7ce4bc8455c2215a6fb3e1b2d
Techassi <git@techassi.dev>
2022-10-15 01:23:55 +0200

    Add endianness_impl macro

--- src/lib.rs
+++ src/lib.rs
@@ -8,7 +8,7 @@ pub use macros::{Readable, Writeable};
 pub use endian::*;
 
 use error::BinaryErrorVariant;
-use macros::from_and_into_bytes_trait_impl;
+use macros::{endianness_impl, from_and_into_bytes_trait_impl};
 
 pub const BITS_IN_BYTE: u32 = 8;
 pub const U16_BYTES: usize = (u16::BITS / BITS_IN_BYTE) as usize;
@@ -47,61 +47,7 @@ pub trait Endianness {
 }
 
 pub struct BigEndian {}
+endianness_impl!(BigEndian, from_be_bytes, to_be_bytes);
 
-impl Endianness for BigEndian {
-    fn read<T: FromBytes>(buf: &[u8]) -> BinaryReadResult<T> {
-        if buf.len() < T::SIZE {
-            return Err(BinaryError::new("Slice of bytes too short", T::ERR_VARIANT));
-        }
-
-        let n = T::from_be_bytes(&buf[..T::SIZE]);
-        Ok(n)
-    }
-
-    fn read_multi<T: FromBytes>(buf: &[u8], nints: usize) -> BinaryReadResult<Vec<T>> {
-        if buf.len() < T::SIZE * nints {
-            return Err(BinaryError::new("Slice of bytes too short", T::ERR_VARIANT));
-        }
-
-        let mut v = Vec::with_capacity(nints);
-
-        for i in 0..nints {
-            match Self::read(&buf[i * T::SIZE..]) {
-                Ok(n) => v.push(n),
-                Err(err) => return Err(err),
-            };
-        }
-
-        Ok(v)
-    }
-
-    fn write<T: IntoBytes>(n: T, buf: &mut [u8]) -> BinaryWriteResult {
-        if buf.len() < T::SIZE {
-            return Err(BinaryError::new("Buf too short", T::ERR_VARIANT));
-        }
-
-        let bytes = n.to_be_bytes();
-
-        for (i, b) in bytes.into_iter().enumerate() {
-            buf[i] = b;
-        }
-
-        Ok(T::SIZE)
-    }
-
-    fn write_multi<T: IntoBytes>(n: Vec<T>, buf: &mut [u8]) -> BinaryWriteResult {
-        let required_len = n.len() * T::SIZE;
-        if buf.len() > required_len {
-            return Err(BinaryError::new("Buf too short", T::ERR_VARIANT));
-        }
-
-        for (i, n) in n.into_iter().enumerate() {
-            match Self::write(n, &mut buf[i * T::SIZE..]) {
-                Err(err) => return Err(err),
-                _ => {}
-            }
-        }
-
-        Ok(required_len)
-    }
-}
+pub struct LittleEndian {}
+endianness_impl!(LittleEndian, from_le_bytes, to_le_bytes);
--- src/macros.rs
+++ src/macros.rs
@@ -41,6 +41,82 @@ macro_rules! from_and_into_bytes_trait_impl {
     };
 }
 
+macro_rules! endianness_impl {
+    ($End:ty, $FromFn:ident, $ToFn:ident) => {
+        impl Endianness for $End {
+            fn read<T: $crate::FromBytes>(buf: &[u8]) -> $crate::BinaryReadResult<T> {
+                if buf.len() < T::SIZE {
+                    return Err($crate::BinaryError::new(
+                        "Slice of bytes too short",
+                        T::ERR_VARIANT,
+                    ));
+                }
+
+                let n = T::$FromFn(&buf[..T::SIZE]);
+                Ok(n)
+            }
+
+            fn read_multi<T: $crate::FromBytes>(
+                buf: &[u8],
+                nints: usize,
+            ) -> $crate::BinaryReadResult<Vec<T>> {
+                if buf.len() < T::SIZE * nints {
+                    return Err($crate::BinaryError::new(
+                        "Slice of bytes too short",
+                        T::ERR_VARIANT,
+                    ));
+                }
+
+                let mut v = Vec::with_capacity(nints);
+
+                for i in 0..nints {
+                    match Self::read(&buf[i * T::SIZE..]) {
+                        Ok(n) => v.push(n),
+                        Err(err) => return Err(err),
+                    };
+                }
+
+                Ok(v)
+            }
+
+            fn write<T: $crate::IntoBytes>(n: T, buf: &mut [u8]) -> $crate::BinaryWriteResult {
+                if buf.len() < T::SIZE {
+                    return Err($crate::BinaryError::new("Buf too short", T::ERR_VARIANT));
+                }
+
+                let bytes = n.$ToFn();
+
+                for (i, b) in bytes.into_iter().enumerate() {
+                    buf[i] = b;
+                }
+
+                Ok(T::SIZE)
+            }
+
+            fn write_multi<T: $crate::IntoBytes>(
+                n: Vec<T>,
+                buf: &mut [u8],
+            ) -> $crate::BinaryWriteResult {
+                let required_len = n.len() * T::SIZE;
+                if buf.len() > required_len {
+                    return Err($crate::BinaryError::new("Buf too short", T::ERR_VARIANT));
+                }
+
+                for (i, n) in n.into_iter().enumerate() {
+                    match Self::write(n, &mut buf[i * T::SIZE..]) {
+                        Err(err) => return Err(err),
+                        _ => {}
+                    }
+                }
+
+                Ok(required_len)
+            }
+        }
+    };
+}
+
 pub(crate) use from_and_into_bytes_trait_impl;
 pub(crate) use from_bytes_trait_impl;
 pub(crate) use into_bytes_trait_impl;
+
+pub(crate) use endianness_impl;
