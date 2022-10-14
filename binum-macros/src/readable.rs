4ab23b95a7068f338f80774933af0744a0da1ab7
Techassi <git@techassi.dev>
2022-10-15 01:24:14 +0200

    Adjust Readable derive macro

--- binum-macros/src/readable.rs
+++ binum-macros/src/readable.rs
@@ -1,6 +1,6 @@
 use proc_macro2::{Ident, Span, TokenStream};
 use quote::{format_ident, quote};
-use syn::{DeriveInput, Error, Result};
+use syn::{punctuated::Punctuated, token::Comma, DeriveInput, Error, Field, Result};
 
 use crate::shared;
 
@@ -20,7 +20,7 @@ pub fn expand(input: DeriveInput) -> Result<TokenStream> {
         }
     };
 
-    // Extract all named fields. THis will return an error if there are unnamed
+    // Extract all named fields. This will return an error if there are unnamed
     // fields present
     let named_fields = match shared::extract_named_fields(struct_data) {
         Some(f) => f.named,
@@ -32,70 +32,115 @@ pub fn expand(input: DeriveInput) -> Result<TokenStream> {
         }
     };
 
-    // These are the allowed / supported types which we can read from the byte slice
-    let allowed_types = vec!["u8", "i8", "u16", "i16", "u32", "i32", "u64", "i64"];
+    if named_fields.len() == 0 {
+        return Ok(quote! {});
+    }
 
-    // Prepare the individual parts of the code gen
-    let mut funcs: Vec<TokenStream> = Vec::new();
-    let mut inner: Vec<TokenStream> = Vec::new();
+    let mut c = TokenStream::new();
 
-    // Iterate over the fields and generate the required code
-    for field in named_fields {
-        let field_name = &field.ident.unwrap();
-
-        let field_type = match shared::extract_last_path_segment_ident(field.ty) {
-            Some(li) => li,
-            None => {
-                return Err(Error::new(
-                    Span::call_site(),
-                    "Failed to extract ident from field type",
-                ))
-            }
+    if named_fields.len() == 1 {
+        c = match gen_one_field(named_fields.first().unwrap(), struct_name) {
+            Ok(ts) => ts,
+            Err(err) => return Err(err),
         };
-
-        // There has to be a better way to do this, right?
-        if !allowed_types.contains(&field_type.to_string().as_str()) {
-            return Err(Error::new(
-                Span::call_site(),
-                format!(
-                    "Invalid type found in struct '{}'. Only {:?} allowed",
-                    input.ident, allowed_types
-                ),
-            ));
-        }
-
-        let var_name = format_ident!("_gen_{}", field_name);
-
-        funcs.push(gen_read_func(&var_name, &field_type));
-
-        inner.push(quote! {
-            #field_name: #var_name,
-        })
+    } else {
+        c = match gen_multiple_fields(named_fields, struct_name) {
+            Ok(ts) => ts,
+            Err(err) => return Err(err),
+        };
     }
 
     Ok(quote! {
         impl #struct_name {
-            /// Read data from the byte slice and populate each of teh struct fields.
-            pub fn read_from(data: &Vec<u8>, endianness: binum::Endianness) -> Result<Self, binum::BinaryError> {
-                let mut offset = 0;
-
-                #(#funcs)*
-
-                return Ok(Self {
-                    #(#inner)*
-                })
+            /// Read data from the byte slice and populate each of the struct fields.
+            pub fn read_from<E>(data: &[u8]) -> Result<Self, binum::BinaryError>
+                where E: binum::Endianness
+            {
+                #c
             }
         }
     })
 }
 
-fn gen_read_func(var_name: &Ident, field_type: &Ident) -> TokenStream {
-    let seek_fn_name = format_ident!("read_seek_{}", field_type);
+/// This generates code when there is only one named field in the struct.
+fn gen_one_field(field: &Field, struct_ident: &Ident) -> Result<TokenStream> {
+    // Extract the field name
+    let field_name = field.ident.as_ref().unwrap();
 
-    quote! {
-        let #var_name = match binum::#seek_fn_name(data, &mut offset, endianness) {
-            Ok(n) => n,
-            Err(err) => return Err(err),
-        };
+    // Extract the field type and also check if it is an allowed type
+    let field_type = match shared::extract_allowed_field_type(&field.ty, struct_ident) {
+        Ok(t) => t,
+        Err(err) => return Err(err),
+    };
+
+    // Construct the variable name
+    let var_name = format_ident!("_gen_{}", field_name);
+    let func = shared::gen_read_func(&var_name, &field_type);
+
+    Ok(quote! {
+        #func
+
+        return Ok(Self {
+            #field_name: #var_name,
+        })
+    })
+}
+
+/// This generates code when there are multiple named fields in the struct.
+fn gen_multiple_fields(
+    fields: Punctuated<Field, Comma>,
+    struct_ident: &Ident,
+) -> Result<TokenStream> {
+    let entries = match shared::extract_continuous_field_types(fields, struct_ident) {
+        Ok(e) => e,
+        Err(err) => return Err(err),
+    };
+
+    // Prepare the individual parts of the code gen
+    let mut funcs: Vec<TokenStream> = Vec::new();
+    let mut inner: Vec<TokenStream> = Vec::new();
+
+    for entry in entries {
+        if entry.count == 1 {
+            let field_type = &entry.ty;
+            let field_name = &entry.idents[0];
+
+            let var_name = format_ident!("_gen_{}", field_name);
+
+            funcs.push(shared::gen_read_func(&var_name, field_type));
+            inner.push(quote! {
+                #field_name: #var_name,
+            });
+        } else {
+            let mut fields = String::new();
+            let field_type = &entry.ty;
+
+            for ident in &entry.idents {
+                fields.push_str(&ident.to_string())
+            }
+
+            let var_name = format_ident!("_gen_multi_{}", fields);
+            funcs.push(shared::gen_multi_read_func(
+                &var_name,
+                field_type,
+                entry.count,
+            ));
+
+            for i in 0..entry.count {
+                let field_name = &entry.idents[i];
+
+                inner.push(quote! {
+                    #field_name: #var_name[#i],
+                })
+            }
+        }
     }
+
+    Ok(quote! {
+        #(#funcs)*
+
+        return Ok(Self {
+            #(#inner)*
+        })
+    })
 }
--- binum-macros/src/shared.rs
+++ binum-macros/src/shared.rs
@@ -1,5 +1,18 @@
-use proc_macro2::Ident;
-use syn::{Data, DataStruct, FieldsNamed, Type};
+use proc_macro2::{Ident, Span, TokenStream};
+use quote::quote;
+use syn::{
+    punctuated::Punctuated, token::Comma, Data, DataStruct, Error, Field, FieldsNamed, Type,
+};
+
+// These are the allowed / supported types which we can read from the byte slice
+pub const ALLOWED_TYPES: [&str; 4] = ["u8", "u16", "u32", "u64"];
+
+#[derive(Debug)]
+pub struct TyEntry {
+    pub idents: Vec<Ident>,
+    pub count: usize,
+    pub ty: Ident,
+}
 
 /// Checks if the provided [`Data`] is a struct and if yes, returns the struct
 /// data as [`DataStruct`].
@@ -20,7 +33,7 @@ pub fn extract_named_fields(strukt: DataStruct) -> Option<FieldsNamed> {
 
 /// Extract the last path segment ident. This is useful to retrieve the type
 /// of a struct field.
-pub fn extract_last_path_segment_ident(ty: Type) -> Option<Ident> {
+pub fn extract_last_path_segment_ident(ty: &Type) -> Option<Ident> {
     match ty {
         Type::Path(p) => {
             if let Some(last) = p.path.segments.last() {
@@ -29,5 +42,89 @@ pub fn extract_last_path_segment_ident(ty: Type) -> Option<Ident> {
             None
         }
         _ => None,
+    }
+}
+
+pub fn extract_allowed_field_type(ty: &Type, struct_ident: &Ident) -> Result<Ident, Error> {
+    let field_type = match extract_last_path_segment_ident(ty) {
+        Some(li) => li,
+        None => {
+            return Err(Error::new(
+                Span::call_site(),
+                "Failed to extract ident from field type",
+            ))
+        }
+    };
+
+    // There has to be a better way to do this, right?
+    if !ALLOWED_TYPES.contains(&field_type.to_string().as_str()) {
+        return Err(Error::new(
+            Span::call_site(),
+            format!(
+                "Invalid type found in struct '{}'. Only {:?} allowed",
+                struct_ident, ALLOWED_TYPES
+            ),
+        ));
+    }
+
+    Ok(field_type)
+}
+
+pub fn extract_continuous_field_types(
+    fields: Punctuated<Field, Comma>,
+    struct_ident: &Ident,
+) -> Result<Vec<TyEntry>, Error> {
+    let mut entries: Vec<TyEntry> = Vec::new();
+
+    for field in fields {
+        let field_type = match extract_allowed_field_type(&field.ty, struct_ident) {
+            Ok(t) => t,
+            Err(err) => return Err(err),
+        };
+
+        if entries.len() == 0 {
+            entries.push(TyEntry {
+                idents: vec![field.ident.unwrap()],
+                count: 1,
+                ty: field_type,
+            });
+            continue;
+        }
+
+        let i = entries.len() - 1;
+
+        if entries[i].ty == field_type {
+            entries[i].idents.push(field.ident.unwrap());
+            entries[i].count += 1;
+            continue;
+        }
+
+        entries.push(TyEntry {
+            idents: vec![field.ident.unwrap()],
+            count: 1,
+            ty: field_type,
+        })
+    }
+
+    return Ok(entries);
+}
+
+/// This generates a single read function call.
+pub fn gen_read_func(var_name: &Ident, field_type: &Ident) -> TokenStream {
+    quote! {
+        let #var_name = match E::read::<#field_type>(data) {
+            Ok(n) => n,
+            Err(err) => return Err(err),
+        };
+    }
+}
+
+/// This generates a multi read function call.
+pub fn gen_multi_read_func(var_name: &Ident, field_type: &Ident, nints: usize) -> TokenStream {
+    quote! {
+        let #var_name = match E::read_multi::<#field_type>(data, #nints) {
+            Ok(n) => n,
+            Err(err) => return Err(err),
+        };
     }
 }
