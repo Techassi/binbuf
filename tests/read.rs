dca01e05cc81e54e122ce8f0cf3b0583d4c016ef
Techassi <git@techassi.dev>
2022-10-15 17:42:09 +0200

    Add read tests

---
+++ tests/read.rs
@@ -0,0 +1,61 @@
+use binum::Endianness;
+
+#[test]
+fn test_read_u16() {
+    let b = vec![69, 88, 65, 77, 80, 76, 69, 33];
+
+    match binum::BigEndian::read::<u16>(&b) {
+        Ok(n) => assert_eq!(n, 17752),
+        Err(err) => panic!("{}", err),
+    };
+}
+
+#[test]
+fn test_read_multi_u16() {
+    let b = vec![69, 88, 65, 77, 80, 76, 69, 33];
+
+    match binum::BigEndian::read_multi::<u16>(&b, 4) {
+        Ok(n) => assert_eq!(n, vec![17752, 16717, 20556, 17697]),
+        Err(err) => panic!("{}", err),
+    };
+}
+
+#[test]
+fn test_read_u32() {
+    let b = vec![69, 88, 65, 77, 80, 76, 69, 33];
+
+    match binum::BigEndian::read::<u32>(&b) {
+        Ok(n) => assert_eq!(n, 1163411789),
+        Err(err) => panic!("{}", err),
+    };
+}
+
+#[test]
+fn test_read_multi_u32() {
+    let b = vec![69, 88, 65, 77, 80, 76, 69, 33];
+
+    match binum::BigEndian::read_multi::<u32>(&b, 2) {
+        Ok(n) => assert_eq!(n, vec![1163411789, 1347175713]),
+        Err(err) => panic!("{}", err),
+    };
+}
+
+#[test]
+fn test_read_u64() {
+    let b = vec![69, 88, 65, 77, 80, 76, 69, 33];
+
+    match binum::BigEndian::read::<u64>(&b) {
+        Ok(n) => assert_eq!(n, 4996815586883028257),
+        Err(err) => panic!("{}", err),
+    };
+}
+
+#[test]
+fn test_read_multi_u64() {
+    let b = vec![69, 88, 65, 77, 80, 76, 69, 33];
+
+    match binum::BigEndian::read_multi::<u64>(&b, 1) {
+        Ok(n) => assert_eq!(n, vec![4996815586883028257]),
+        Err(err) => panic!("{}", err),
+    };
+}
