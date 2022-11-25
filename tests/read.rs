65d89252b45fffa13f2b01124073869028a1c9dc
Techassi <git@techassi.dev>
2022-11-25 15:50:11 +0100

    Add `read_u128` test

--- tests/read.rs
+++ tests/read.rs
@@ -45,6 +45,19 @@ fn test_read_u64() {
 }
 
 #[test]
+fn test_read_u128() {
+    let b = vec![
+        69, 88, 65, 77, 80, 76, 69, 33, 69, 88, 65, 77, 80, 76, 69, 33,
+    ];
+    let mut b = ReadBuffer::new(b.as_slice());
+
+    match u128::read::<BigEndian>(&mut b) {
+        Ok(n) => assert_eq!(n, 92174978314754016623629927450611041569),
+        Err(err) => panic!("{}", err),
+    }
+}
+
+#[test]
 fn test_read_char_string() {
     let b = vec![8, 69, 88, 65, 77, 80, 76, 69, 33];
     let mut b = ReadBuffer::new(b.as_slice());
