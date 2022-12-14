ce5b72f0f17b76644c8d044fb5b9b6384302d586
Techassi <git@techassi.dev>
2022-12-14 19:58:14 +0100

    Add `Readable` impl test

---
+++ tests/read_impl.rs
@@ -0,0 +1,24 @@
+use binbuf::prelude::*;
+
+#[test]
+fn test_readable_impl() {
+    struct Data {
+        inner: u16,
+    }
+
+    impl<'a> Readable<'a> for Data {
+        type Error = BufferError;
+
+        fn read<E: Endianness>(buf: &mut impl ToReadBuffer<'a>) -> Result<Self, Self::Error> {
+            let inner = u16::read::<E>(buf)?;
+
+            Ok(Self { inner })
+        }
+    }
+
+    let b = vec![69, 88, 65, 77, 80, 76, 69, 33];
+    let mut buf = ReadBuffer::new(b.as_slice());
+
+    let data = Data::read::<BigEndian>(&mut buf).unwrap();
+    assert_eq!(data.inner, 17752)
+}
