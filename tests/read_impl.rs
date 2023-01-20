e81343b647e53cc236e364591185144171c1ff5f
Techassi <git@techassi.dev>
2023-01-20 16:56:16 +0100

    Add Read and write derive macro tests

--- tests/read_impl.rs
+++ tests/read_impl.rs
@@ -22,3 +22,39 @@ fn test_readable_impl() {
     let data = Data::read::<BigEndian>(&mut buf).unwrap();
     assert_eq!(data.inner, 17752)
 }
+
+#[cfg(feature = "derive")]
+#[test]
+fn test_readable_impl_derive_simple() {
+    #[derive(Read)]
+    struct Data {
+        inner: u16,
+    }
+
+    let b = vec![69, 88, 65, 77, 80, 76, 69, 33];
+    let mut buf = ReadBuffer::new(b.as_slice());
+
+    let data = Data::read::<BigEndian>(&mut buf).unwrap();
+    assert_eq!(data.inner, 17752)
+}
+
+#[cfg(feature = "derive")]
+#[test]
+fn test_readable_impl_derive_more_fields() {
+    use std::net::Ipv4Addr;
+
+    #[derive(Read)]
+    struct Data {
+        v1: u16,
+        v2: u16,
+        v3: Ipv4Addr,
+    }
+
+    let b = vec![69, 88, 65, 77, 80, 76, 69, 33];
+    let mut buf = ReadBuffer::new(b.as_slice());
+
+    let data = Data::read::<BigEndian>(&mut buf).unwrap();
+    assert_eq!(data.v1, 17752);
+    assert_eq!(data.v2, 16717);
+    assert_eq!(data.v3, Ipv4Addr::new(80, 76, 69, 33));
+}
--- tests/write_impl.rs
+++ tests/write_impl.rs
@@ -25,3 +25,51 @@ fn test_writeable_impl() {
         Err(err) => panic!("{}", err),
     };
 }
+
+#[cfg(feature = "derive")]
+#[test]
+fn text_writeable_impl_derive_simple() {
+    #[derive(Write)]
+    struct Data {
+        inner: u16,
+    }
+
+    let d = Data { inner: 17752 };
+    let mut b = WriteBuffer::new();
+
+    match d.write::<BigEndian>(&mut b) {
+        Ok(n) => {
+            assert_eq!(n, 2);
+            assert_eq!(b.bytes(), &[69, 88]);
+        }
+        Err(err) => panic!("{}", err),
+    };
+}
+
+#[cfg(feature = "derive")]
+#[test]
+fn text_writeable_impl_derive_more_fields() {
+    use std::net::Ipv4Addr;
+
+    #[derive(Write)]
+    struct Data {
+        v1: u16,
+        v2: u16,
+        v3: Ipv4Addr,
+    }
+
+    let d = Data {
+        v1: 17752,
+        v2: 16717,
+        v3: Ipv4Addr::new(80, 76, 69, 33),
+    };
+    let mut b = WriteBuffer::new();
+
+    match d.write::<BigEndian>(&mut b) {
+        Ok(n) => {
+            assert_eq!(n, 8);
+            assert_eq!(b.bytes(), &[69, 88, 65, 77, 80, 76, 69, 33]);
+        }
+        Err(err) => panic!("{}", err),
+    };
+}
